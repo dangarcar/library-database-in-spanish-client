7909d1fa7ce555e9d84581c4a28f635ede17da7f
Daniel García <danielgarciacarrete@gmail.com>
2022-04-27 21:12:33 +0200

    Se ha añadido el método para buscar los perfiles en la base de datos y se han añadido los correspondientes triggers para la base de datos

--- src/main/java/SQL/Buscador.java
+++ src/main/java/SQL/Buscador.java
@@ -7,9 +7,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import contenido.Contenido;
 import contenido.excepciones.ExcepcionContenido;
+import perfiles.Perfil;
 
 public class Buscador {
-	public static ArrayList<Contenido> buscar(String busqueda) throws ExcepcionContenido {
+	/**
+	 * Hace una búsqueda de contenido basada en el String pasado por parámetro
+	 * @param busqueda texto en el que se basa la búsqueda
+	 * @return La lista de contenidos que coinciden conesa búsqueda
+	 * @throws ExcepcionContenido
+	 */
+	public static ArrayList<Contenido> buscarContenido(String busqueda) throws ExcepcionContenido {
 		ConectorSQL conector = null;
 		ResultSet resultado = null;
 		Connection conexion = null;
@@ -49,5 +56,47 @@ public class Buscador {
 		}
 		
 		return contenidos;
+	}
+	
+	/**
+	 * Hace una búsqueda de perfiles basada en el String pasado por parámetro
+	 * @param busqueda texto en el que se basa la búsqueda
+	 * @return La lista de perfiles que coinciden con esa búsqueda
+	 */
+	public static ArrayList<Perfil> buscarPerfiles(String busqueda){
+		ConectorSQL conector = null;
+		ResultSet resultado = null;
+		Connection conexion = null;
+		PreparedStatement st = null;
+		ArrayList<Perfil> perfiles = new ArrayList<Perfil>();
+		
+		String peticionSQL = "SELECT DNI,Nombre,Apellidos,CorreoElectronico FROM BusquedaPerfiles(?) ORDER BY rank;";	
+		
+		try {
+			//Creo el objeto con el que conectarme a la BD
+			conector = new ConectorSQL();
+			conexion = conector.conectar();
+			
+			//Hago al peticion SQL a la base de datos y almaceno el ResultSet
+			st = conexion.prepareStatement(peticionSQL);
+			st.setString(1, busqueda);
+			resultado = st.executeQuery();
+			st.clearParameters();
+			
+			while(resultado.next()) {
+				perfiles.add(PerfilSQL.getPerfil(resultado.getInt("DNI")));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			//throw new ExcepcionContenido("Hubo un error con la base de datos, es probable que el contenido que buscas no exista en esta biblioteca",contenidos.get(0));
+		} finally {
+			//Si la conexion a la base de datos existe, la cierro
+			if(conector != null) {
+				conector.cerrar();
+			}
+		}
+		
+		return perfiles;
 	}
 }
